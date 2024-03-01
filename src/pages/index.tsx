9bb02a01bc81f952f7d54e81334a4c827b67a271
chiaraferrara <chiara.ferrara@stevejobs.academy>
2024-03-01 11:10:41 +0100

    feat: getServerSideProps retrieves products list from internal API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,13 @@ interface Product{
 
 }
 
+
+//la chiamata avviene in runtime, quindi non è possibile fare il fetch in fase di build
+//se invece fosse stato getStaticProps, sarebbe stato possibile, i dati sono aggiornati nel momento in cui la pagina viene buildata
+//(quando si fa run build, i dati vengono fetchati e se il server è giù, la pagina non viene visualizzata)
+//SSG significa Server Side Generation
+
+//su vercel potremmo avere problemi, il server non si trova l'api attiva. 
 export const getServerSideProps = async () => {
   try{
   const res = await fetch("https://nodejs-api-production-0c7a.up.railway.app/");
@@ -17,6 +24,10 @@ console.log(products);
     };
   }
 }
+
+//VANTAGGI STATIC PROPS: quando carico sul server la pagina già con i dati, la pagina viene visualizzata molto più velocemente
+//VANTAGGI SERVER SIDE PROPS: i dati sono sempre aggiornati, non importa se il server è giù, i dati vengono sempre fetchati
+
 
 interface Props {
   products: Product[]
